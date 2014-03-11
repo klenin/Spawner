219f44b282c1ee2640ad357a34ea23bc58012bae
Pete Khrapchenkov <shigidono@gmail.com>
2014-03-11 19:39:03 +1100

    Fix support for old spawner and add comparison function

--- test/spawner.pm
+++ test/spawner.pm
@@ -3,24 +3,6 @@ use Cwd 'abs_path';
 use IPC::Run qw(start finish);
 use Data::Dumper;
 
-
-our @EXPORT = qw(sp_old sp_new parse_report);
-my $sp_ext = '.exe';
-
-sub path_to_system_specific {
-    return catfile(split('/', $_[0]));
-}
-
-my $old_sp = path_to_system_specific(abs_path('../bin/old/sp'.$sp_ext));
-my $new_sp = path_to_system_specific(abs_path('../bin/new/sp'.$sp_ext));
-
-sub sp_old {
-    my ($params, %rest) = @_;
-    my $h = start [$new_sp, split(' ', $params)], \$in, \$out, \$err;
-    $?==0 or die "Spawner execution finished with $?";
-    finish $h or die;
-    return $out;
-}
 my @required_fields = (
     'Application', 
     'Parameters', 
@@ -38,6 +20,37 @@ my @required_fields = (
     'ExitStatus',
     'SpawnerError'
 );
+
+our @EXPORT = qw(sp_old sp_new parse_report sp_compete);
+my $sp_ext = '.exe';
+
+sub path_to_system_specific {
+    return catfile(split('/', $_[0]));
+}
+
+my $old_sp = path_to_system_specific(abs_path('../bin/old/sp'.$sp_ext));
+my $new_sp = path_to_system_specific(abs_path('../bin/new/sp'.$sp_ext));
+
+sub sp_from_disk {
+    open REPORT, "<$_[0]";
+    open OUT, "<$_[1]";
+    my $report = do { local $/; <REPORT> };    
+    my $stdout = do { local $/; <OUT> };    
+    return ($report, $stdout);
+}
+sub sp_old {
+    my ($params, %rest) = @_;
+    my $status = system("$old_sp -so:stdout.txt -sr:report.txt -hr:1 $params") >> 8;
+    $status == 0 or die "Old spawner execution finished with $status";
+    return sp_from_disk("report.txt", "stdout.txt");
+}
+sub sp_new {
+    my ($params, %rest) = @_;
+    my $h = start [$new_sp, split(' ', $params)], \$in, \$out, \$err;
+    $?==0 or die "New spawner execution finished with $?";
+    finish $h or die;
+    return $out;
+}
 sub parse_report {
     my ($report, $check) = @_;
     my %report_object = {};
@@ -73,6 +86,7 @@ sub parse_report {
             }
             if ($check) {
                 if ($p != $required_fields[$checking]) {
+                    print "Wrong position for $p\n";
                     return undef;
                 }
                 $checking++;
@@ -82,6 +96,15 @@ sub parse_report {
     }
     #print Dumper(\%report_object);
     return %report_object;
+}
+
+sub sp_compete {
+    my ($params, $check) = @_;
+    my ($report, $stdout) = sp_old($params);
+
+    my %old_report = parse_report($report, $check);
+    my %new_report = parse_report(sp_new($params), $check);
+    return (\%old_report, \%new_report);
 }
 
 
