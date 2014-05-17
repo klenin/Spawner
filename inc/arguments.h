7375bddeb2429cb736cd538567e9cad7ea8e035c
Pete Khrapchenkov <shigidono@gmail.com>
2014-05-17 22:21:52 +1100

    Start implementation of described behavior  * command line argument parsing

--- inc/arguments.h
+++ inc/arguments.h
@@ -6,6 +6,8 @@
 #include <map>
 #include <vector>
 
+#define min_def(x, y)((x)<(y)?(x):(y))
+#define max_def(x, y)((x)>(y)?(x):(y))
 class argument_value_c {
 public:
     static argument_value_c *create_argument(const std::string &value) {
@@ -27,22 +29,6 @@ private:
     std::map<std::string, temp_name> contents;
 public:
     void add_value(const std::string &key, temp_name &value);
-};
-
-
-//configuration_manager
-class argument_parser_c {
-private:
-    dictionary_c short_names;
-    dictionary_c long_names;
-    dictionary_c environment_names;
-public:
-    template<typename T> static argument_value_c *create_value(const std::string &value) {
-        return new T(value);
-    }
-
-    void add_dictionary(temp_name *dictionary);
-    void parse(int argc, char *argv[]);
 };
 
 enum on_error_behavior_e {
@@ -75,6 +61,47 @@ enum behavior_type_e {
     variable_behavior
 };
 
+class abstract_settings_parser_c {
+public:
+    virtual char *get_next_argument() = 0;
+};
+
+class abstract_parser_c {
+public:
+    virtual bool invoke_initialization(abstract_settings_parser_c &parser_object){return false;} //init settings for parser object
+    virtual bool parse(abstract_settings_parser_c &parser_object) {return false;}
+    virtual void invoke(abstract_settings_parser_c &parser_object) {}
+    virtual std::string help() { return ""; }
+};
+
+//configuration_manager
+class settings_parser_c: public abstract_settings_parser_c {
+private:
+    int position;
+    int fetched_position;
+    void *system_parser;
+    std::vector<std::pair<int, abstract_parser_c*> > saved_positions;
+    std::vector<abstract_parser_c> parsers;
+    int arg_c;
+    char **arg_v;
+    //std::vector<int> system_parsers;
+public:
+    settings_parser_c() {}
+    template<typename T> static argument_value_c *create_value(const std::string &value) {
+        return new T(value);
+    }
+    int current_position();
+    void fetch_current_position();
+    void restore_position();
+    bool system_parse();
+    size_t saved_count();
+    char *get_next_argument();
+    void save_current_position(abstract_parser_c *associated_parser);
+    abstract_parser_c *pop_saved_parser();
+    void parse(int argc, char *argv[]);
+};
+
+
 struct argument_t {
     char *name;
     argument_value_c *(*callback)(const std::string&);
@@ -83,12 +110,13 @@ struct argument_t {
     char *help;
 };
 
-#define VALUE_CONSTRUCTOR(X) &argument_parser_c::create_value<X>
+#define VALUE_CONSTRUCTOR(X) &settings_parser_c::create_value<X>
+typedef char *argument_type_t;
 
 argument_t base_dictionary[] = {
     {
         "output_stream",
-        VALUE_CONSTRUCTOR(argument_stream_value),
+        VALUE_CONSTRUCTOR(argument_value_c),
         string_value, //expected value
         array_behavior,
         "help"
@@ -99,24 +127,71 @@ argument_t base_dictionary[] = {
 
 
 
-class abstract_parser_c {
-public:
-    virtual bool invoke_initialization(parser_object){return false;} //init settings for parser object
-    virtual bool invoke(parser_object) {return false;}
-    virtual std::string help() { return ""; }
-};
 
 class console_argument_parser_c : public abstract_parser_c {
+private:
+    enum parsing_state_e {
+        argument_started_state,
+        argument_ok_state,
+        argument_error_state,
+    };
+    std::string argument;
+    std::vector<std::string> values;
+    char *argument_name;
+    parsing_state_e last_state;
 public:
-    virtual bool invoke(parser_object) {
-        while (process(parser_object.get_next_argument()));
+    virtual bool parse(abstract_settings_parser_c &parser_object) {
+        if (process_argument(parser_object.get_next_argument()) == argument_error_state) {
+            return false;
+        }
+        while (process_value(parser_object.get_next_argument()) == argument_started_state);
+        return last_state == argument_ok_state;
+    }
+    virtual parsing_state_e process_argument(const char *argument) {
+        //check if in the dictionary
+        if (!argument) {
+            return last_state = argument_error_state;
+        }
+        std::vector<std::string> dividers;
+        std::string s = argument;
+        std::string left, right;
+        size_t length = s.length();
+        size_t divider_length = 0;
+        dividers.push_back("=");
+        dividers.push_back(":");
+        for (auto i = dividers.begin(); i != dividers.end(); i++) {
+            size_t pos = s.find(*i);
+            length = min_def(length, pos);
+            if (length == pos) {
+                divider_length = max_def(divider_length, (*i).length());
+            }
+        }
+        left = s.substr(0, length);
+        //check left
+        last_state = argument_started_state;
+        if (length < s.length()) {
+            right = s.substr(length + divider_length, s.length() - length - divider_length - 1);
+            return process_value(right.c_str());
+        }
+        return last_state;
+    }
+    virtual parsing_state_e process_value(const char *argument) {
+        if (last_state == argument_ok_state) {
+            return last_state;
+        }
+        if (!argument) {
+            return last_state = argument_error_state;
+        }
+        return last_state;
+    }
+    virtual void invoke(abstract_settings_parser_c &parser_object) {
     }
     virtual std::string help() { return ""; }
 };
 
 class environment_variable_parser_c : public abstract_parser_c {
 protected:
-    std::map<argument_type, std::string> values;
+    std::map<argument_type_t, std::string> values;
 public:
     environment_variable_parser_c(dictionary) {
         foreach (item in dictionary) {
@@ -125,31 +200,12 @@ public:
             }
         }
     }
-    virtual bool invoke_initialization(parser_object){
+    virtual bool invoke_initialization(abstract_settings_parser_c &parser_object){
         return true;
     }
 };
 
-parse () {
-    while (arguments_exists) {
-        bool already_parsed = system_parse();
-        foreach (parser in parsers) {
-            fetch_position();
-            bool parsed = parser.invoke(this);
-            if (parsed && already_parsed) {
-                conflict;
-            }
-            already_parsed = parsed;
-        }
-        if (!already_parsed) {
-            if (is_program) {
-                parse_program
-            } else {
-                unknown_argument
-            }
-        }
-    }
-}
+
 
 
 
--- src/arguments.cpp
+++ src/arguments.cpp
@@ -12,14 +12,6 @@ void dictionary_c::add_value(const std::string &key, temp_name &value) {
     contents[key] = value;
 }
 
-void argument_parser_c::add_dictionary(temp_name *dictionary) {
-    while ((*dictionary).callback) {
-        short_names.add_value((*dictionary).short_name, *dictionary);
-        long_names.add_value((*dictionary).long_name, *dictionary);
-        environment_names.add_value((*dictionary).environment_name, *dictionary);
-        dictionary++;
-    }
-}
 
 std::pair<std::string, std::string> extract_argument(const std::string &s) {
     std::map<std::string, bool> dividers;
@@ -35,30 +27,68 @@ std::pair<std::string, std::string> extract_argument(const std::string &s) {
     return std::pair<std::string, std::string>(left, right);
 }
 
-void argument_parser_c::parse(int argc, char *argv[]) {
-    for (int i = 1; i < argc; ++i) {
-        std::string argument = argv[i];
-        std::cout << argument << std::endl;
-        if (argument[0] == '-') {
-            if (argument.length() > 1) {
-                if (argument[1] == '-') {
-                    //long argument
-                    std::pair<std::string, std::string> arg = extract_argument(argument.substr(2, argument.length() - 1));
-                    std::cout << "\"" << arg.first << "\" = \"" << arg.second << "\"" << std::endl;
-                    std::cout << "long argument" << std::endl;
-                } else {
-                    //short argument
-                    std::pair<std::string, std::string> arg = extract_argument(argument.substr(1, argument.length() - 1));
-                    std::cout << "\"" << arg.first << "\" = \"" << arg.second << "\"" << std::endl;
-                    std::cout << "short argument" << std::endl;
-                }
-            } else {
-                //strange symbol
-                std::cout << "shit" << std::endl;
+int settings_parser_c::current_position() {
+    return position;
+}
+void settings_parser_c::fetch_current_position() {
+    fetched_position = position;
+}
+void settings_parser_c::restore_position() {
+    position = fetched_position;
+}
+bool settings_parser_c::system_parse() {
+    return false;
+}
+size_t settings_parser_c::saved_count() {
+    return saved_positions.size();
+}
+char *settings_parser_c::get_next_argument() {
+    if (position >= arg_c) {
+        return NULL;
+    }
+    return arg_v[position];
+}
+void settings_parser_c::save_current_position(abstract_parser_c *associated_parser) {
+    saved_positions.push_back(std::pair<int, abstract_parser_c*>(current_position(), associated_parser));
+}
+abstract_parser_c *settings_parser_c::pop_saved_parser() {
+    auto value = saved_positions.front();//std::pair<int, void*>
+    position = value.first;
+    saved_positions.erase(saved_positions.begin(), saved_positions.begin());
+    return value.second;
+}
+
+
+void settings_parser_c::parse(int argc, char *argv[]) {
+    arg_c = argc;
+    arg_v = argv;
+    for (auto parser = parsers.begin(); parser != parsers.end(); parser++) {
+        (*parser).invoke_initialization(*this);
+    }
+    while (current_position() < argc) {
+        fetch_current_position();
+        if (system_parse()) {
+            //save_current_position(&system_parser);
+        }
+        for (auto parser = parsers.begin(); parser != parsers.end(); parser++) {
+            fetch_current_position();
+            if ((*parser).parse(*this)) {
+                save_current_position(&(*parser));
             }
+            restore_position();
+        }
+        if (saved_count() == 1) {
+            pop_saved_parser()->invoke(*this);
+        } else if (saved_count() > 1) {
+            //ambiguous arguments
+            //throw
         } else {
-            //
-            std::cout << "unknown" << std::endl;
+            if (false) {//is_program()) {
+                //parse_program//until separator detected
+            } else {
+                //unknown_argument
+                //throw
+            }
         }
     }
 }
