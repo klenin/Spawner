fdf4724cee9240a062391f2afe1eef5d59d80fb0
Dmitry D. Chernov <blackdoomer@yandex.ru>
2017-05-02 20:18:40 +1000

    options: Group and reorder class fields.

--- libspawner/inc/options.h
+++ libspawner/inc/options.h
@@ -8,13 +8,46 @@
 
 // hide_gui - creates process with hidden flag turned on
 // silent_errors - on windows, if error occurs program doesn't show "send report" dialog
-// #to do: make some options as map of variant
+
+// TODO: make some options as map of variant
+// TODO: rewrite as a class
 
 struct options_class
 {
     static const unsigned SHARED_MEMORY_BUF_SIZE = 4069;
 
     session_class session;
+    std::string report_file;  /* bad, need many values
+                                 but this causes many outputs if environment
+                                 variable and command line argument both present */
+    std::string shared_memory;
+    
+    std::string string_arguments;
+    std::string working_directory;
+    
+    std::string login;
+    std::string password;
+    
+    std::vector<std::string> stdinput;
+    std::vector<std::string> stdoutput;
+    std::vector<std::string> stderror;
+    std::list<std::string> arguments;
+
+    std::list< std::pair< std::string, std::string > > environmentVars;
+    std::string environmentMode;
+       
+    bool hide_gui;
+    bool hide_report;
+    bool hide_output;
+    bool debug;
+    bool json;
+    bool secure_token;
+    bool silent_errors;
+    bool use_cmd;
+    bool delegated;
+    bool controller;
+    unsigned long monitorInterval;
+ 
     options_class(const options_class &options);
     options_class(const session_class &session_param)
         : hide_gui(true)
@@ -30,6 +63,7 @@ struct options_class
         , json(false)
         , controller(false)
         , environmentMode("inherit") {}
+
     void add_argument(std::string argument);
     void add_arguments(const std::vector<std::string> &arguments_a);
     void push_argument_front(std::string argument);
@@ -44,29 +78,6 @@ struct options_class
     std::string get_argument(size_t index) const;
     size_t get_arguments_count() const;
     std::string format_arguments() const;
-    std::string string_arguments;
-    std::string working_directory;
-    std::string login;
-    std::string password;
-    std::vector<std::string> stdinput;
-    std::vector<std::string> stdoutput;
-    std::vector<std::string> stderror;
-    std::list< std::pair< std::string, std::string > > environmentVars;
-    std::list<std::string> arguments;
-    std::string report_file; //bad, need many values but this causes many outputs if environment variable and command line argument both present
-    std::string shared_memory;
-    bool hide_gui;
-    bool hide_report;
-    bool hide_output;
-    bool debug;
-    bool json;
-    bool secure_token;
-    bool silent_errors;
-    bool use_cmd;
-    bool delegated;
-    bool controller;
-    unsigned long monitorInterval;
-    std::string environmentMode;
 };
 
 #endif //_SPAWNER_OPTIONS_H_
